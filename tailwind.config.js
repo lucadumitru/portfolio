12264f9f1daf850b30a586daec5d50ad434b1d1e
lucadumitru <luca.dumitru25@gmail.com>
2023-09-26 18:13:30 +0200

    try error layout fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
 	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	darkMode: 'class',
+	darkMode: "class",
 	theme: {
 		extend: {
 			colors: {
-				gray: '#666',
-				textSecondary: '#42446E',
-				bgDark: '#191919',
+				gray: "#666",
+				textSecondary: "#42446E",
+				bgDark: "#191919",
 			},
 			boxShadow: {
-				card: '2px 2px 100px 0px rgba(0, 0, 0, 0.20)',
+				card: "2px 2px 100px 0px rgba(0, 0, 0, 0.20)",
 			},
 		},
 	},
