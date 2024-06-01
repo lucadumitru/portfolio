f4facf11692d54c38a6ca8b08a57998025ae003a
lucadumitru <luca.dumitru25@gmail.com>
2024-06-01 16:31:39 +0200

    fix(constants): fixed production base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
 	process.env.NODE_ENV === 'production'
-		? 'https://portfolio-git-dev-lucadumitrus-projects.vercel.app/'
+		? 'https://portfolio-git-dev-lucadumitrus-projects.vercel.app'
 		: 'http://localhost:3000';
