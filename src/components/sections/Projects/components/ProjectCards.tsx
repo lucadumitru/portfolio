3e9a4c79cfa009ff1f70488453bd16ec46fe795b
lucadumitru <luca.dumitru25@gmail.com>
2025-01-13 13:26:58 +0100

    feat(project-cards): removed lazy-motion

--- src/components/sections/Projects/components/ProjectCards.tsx
+++ src/components/sections/Projects/components/ProjectCards.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { domAnimation, LazyMotion } from 'framer-motion';
 import { useState } from 'react';
+
 import { Card } from '@/components/ui';
 
 interface ProjectCardsProps {
@@ -21,20 +21,19 @@ export const ProjectCards = ({ blurredImages, projects }: ProjectCardsProps) => 
 
 	return (
 		<>
-			<LazyMotion features={domAnimation}>
-				<div
-					className={`mt-[25px] grid grid-cols-1 gap-[45px] gap-y-[20px] md:mt-[80px] md:grid-cols-2 md:gap-y-[40px] lg:grid-cols-3 xl:mt-[120px]`}
-				>
-					{projects.slice(0, projectsShowLength).map((project, index) => (
-						<Card
-							key={project.id}
-							blurredImage={blurredImages[index]}
-							index={index}
-							project={project}
-						/>
-					))}
-				</div>
-			</LazyMotion>
+			<div
+				className={`mt-[25px] grid grid-cols-1 gap-[45px] gap-y-[20px] md:mt-[80px] md:grid-cols-2 md:gap-y-[40px] lg:grid-cols-3 xl:mt-[120px]`}
+			>
+				{projects.slice(0, projectsShowLength).map((project, index) => (
+					<Card
+						key={project.id}
+						blurredImage={blurredImages[index]}
+						index={index}
+						project={project}
+					/>
+				))}
+			</div>
+
 			{projectsShowLength <= projects.length && (
 				<button
 					className='text-md before:content-[" "] relative mt-10 flex cursor-pointer items-center justify-center place-self-center overflow-hidden whitespace-nowrap rounded-[100px] border border-gray bg-white from-cyan-500 to-fuchsia-500 p-4 transition-all before:absolute before:h-full before:w-full before:bg-gradient-to-r before:opacity-0 before:transition-opacity hover:border-white hover:text-white hover:before:opacity-100 dark:bg-bgDark sm:inline-flex md:w-auto md:p-5 md:text-lg md:hover:no-underline'
