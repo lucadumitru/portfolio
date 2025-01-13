6e96cca952c6349d400e49da0b28ea01c78c88aa
lucadumitru <luca.dumitru25@gmail.com>
2025-01-13 12:52:24 +0100

    feat(utils): changed cn function to arrow function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,9 +4,9 @@ import fs from 'node:fs/promises';
 import { getPlaiceholder } from 'plaiceholder';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
-}
+};
 
 export const getBase64 = async (imgPath: string) => {
 	try {
