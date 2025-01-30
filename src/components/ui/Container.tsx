f58900dbb0f29216fdb713a14e84b6ec8dc3d995
lucadumitru <luca.dumitru25@gmail.com>
2025-01-30 16:59:57 +0100

    refactor(container): refactored container component

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,9 +1,14 @@
 import { cn } from '@/src/lib/utils';
 
-interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+interface ContainerProps extends React.HTMLAttributes<HTMLElement> {
+	as?: React.HTMLElementType;
 	children: React.ReactNode;
 }
 
-export const Container = ({ children, className }: ContainerProps) => {
-	return <div className={cn('mx-auto max-w-[75rem] px-4', className)}>{children}</div>;
+export const Container = ({ children, as: Tag = 'div', className, ...props }: ContainerProps) => {
+	return (
+		<Tag {...props} className={cn('mx-auto w-full max-w-screen-xl px-4', className)}>
+			{children}
+		</Tag>
+	);
 };
